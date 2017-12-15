78fb56f3cf244173a11bfb9e7465b969ff0b3dca
Muhtar Arfa <muhtarrn@gmail.com>
2017-12-15 10:33:23 +0700

    kelola data santri (Update & delete)

--- app/src/main/java/com/example/asus_pc/monitoringhafalanapp2/UbahDataActivity.java
+++ app/src/main/java/com/example/asus_pc/monitoringhafalanapp2/UbahDataActivity.java
@@ -11,11 +11,18 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.eyro.mesosfer.GetCallback;
+import com.eyro.mesosfer.MesosferData;
+import com.eyro.mesosfer.MesosferException;
+import com.eyro.mesosfer.MesosferObject;
+import com.eyro.mesosfer.SaveCallback;
+
 public class UbahDataActivity extends AppCompatActivity {
-    protected Cursor cursor;
+    private String objectId;
     DataSantri dbSantri;
     Button ubah;
     EditText text1, text2, text3, text4, text5, text6;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,13 +38,15 @@ public class UbahDataActivity extends AppCompatActivity {
         ubah = (Button) findViewById(R.id.simpan);
 
         Intent i = getIntent();
-        text1.setText(i.getStringExtra(Config.NOMOR_INDUK));
+        objectId = i.getStringExtra("id");
+        /*text1.setText(i.getStringExtra(Config.NOMOR_INDUK));
         text2.setText(i.getStringExtra(Config.NAMA_SANTRI));
         text3.setText(i.getStringExtra(Config.KELAS));
         text4.setText(i.getStringExtra(Config.KONSULAT));
         text5.setText(i.getStringExtra(Config.NAMA_WALI));
-        text6.setText(i.getStringExtra(Config.NO_TELEPON));
+        text6.setText(i.getStringExtra(Config.NO_TELEPON));*/
 
+        getDatabyId(objectId);
 
         ubah.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -47,9 +56,7 @@ public class UbahDataActivity extends AppCompatActivity {
                         .setCancelable(false)
                         .setPositiveButton("Ya",new DialogInterface.OnClickListener() {
                             public void onClick(DialogInterface dialog, int id) {
-                                Toast.makeText(getApplicationContext(), "Data Berhasil Diubah", Toast.LENGTH_SHORT).show();
-                                Intent intent = new Intent(UbahDataActivity.this, KelolaSantriActivity.class);
-                                startActivity(intent);
+                                updateDataSantri(objectId);
                             }
                         })
                         .setNegativeButton("Tidak",new DialogInterface.OnClickListener() {
@@ -59,6 +66,48 @@ public class UbahDataActivity extends AppCompatActivity {
                         });
                 AlertDialog alert = ab.create();
                 alert.show();
+            }
+        });
+    }
+
+    private void getDatabyId(String objectId){
+        MesosferData.createWithObjectId(objectId).fetchAsync(new GetCallback<MesosferData>() {
+            @Override
+            public void done(MesosferData mesosferData, MesosferException e) {
+                MesosferObject mesosferObject = mesosferData.getData();
+                text1.setText(mesosferObject.optString(Config.NOMOR_INDUK));
+                text2.setText(mesosferObject.optString(Config.NAMA_SANTRI));
+                text3.setText(mesosferObject.optString(Config.KELAS));
+                text4.setText(mesosferObject.optString(Config.KONSULAT));
+                text5.setText(mesosferObject.optString(Config.NAMA_WALI));
+                text6.setText(mesosferObject.optString(Config.NO_TELEPON));
+            }
+        });
+    }
+
+    private void updateDataSantri(String objectId){
+        MesosferData.createWithObjectId(objectId).fetchAsync(new GetCallback<MesosferData>() {
+            @Override
+            public void done(MesosferData mesosferData, MesosferException e) {
+                MesosferData.createData("Santri");
+                mesosferData.setData(Config.NOMOR_INDUK, text1.getText().toString());
+                mesosferData.setData(Config.NAMA_SANTRI, text2.getText().toString());
+                mesosferData.setData(Config.KELAS, text3.getText().toString());
+                mesosferData.setData(Config.KONSULAT, text4.getText().toString());
+                mesosferData.setData(Config.NAMA_WALI, text5.getText().toString());
+                mesosferData.setData(Config.NO_TELEPON, text6.getText().toString());
+                mesosferData.saveAsync(new SaveCallback() {
+                    @Override
+                    public void done(MesosferException e) {
+                        if (e != null){
+                            Toast.makeText(UbahDataActivity.this, "update data santri gagal", Toast.LENGTH_SHORT).show();
+                        } else {
+                                Toast.makeText(getApplicationContext(), "update data santri berhasil", Toast.LENGTH_SHORT).show();
+                                Intent intent = new Intent(UbahDataActivity.this, KelolaSantriActivity.class);
+                                startActivity(intent);
+                        }
+                    }
+                });
             }
         });
     }
